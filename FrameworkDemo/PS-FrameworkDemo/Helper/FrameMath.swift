98adc261b2efeca8683b889530072c19f6a07db6
Bhumit012 <bmuchhadia@yahoo.com>
2023-02-11 22:28:09 -0600

    Framework

---
+++ FrameworkDemo/PS-FrameworkDemo/Helper/FrameMath.swift
@@ -0,0 +1,14 @@
+//
+//  Maths.swift
+//  PS-FrameworkDemo
+//
+//  Created by Bhumit Muchhadia on 2023-01-16.
+//
+
+import Foundation
+
+open class FrameMath {
+    static func addNumbers(a: Int, b: Int) -> Int {
+        return MathHelper.mathHelp(a: a, b: b)
+    }
+}
---
+++ FrameworkDemo/PS-FrameworkDemo/Helper/MathHelper.swift
@@ -0,0 +1,14 @@
+//
+//  MathHelper.swift
+//  PS-FrameworkDemo
+//
+//  Created by Bhumit Muchhadia on 2023-01-16.
+//
+
+import Foundation
+
+internal class MathHelper {
+    static func mathHelp(a: Int, b: Int) -> Int {
+        return a + b
+    }
+}
